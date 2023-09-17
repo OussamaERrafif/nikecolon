06ef2877650c6020111957f90ea1efae85194429
OussamaERrafif <oussamaerra2002@gmail.com>
2023-09-17 17:11:26 +0100

    v1

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProduct from "./PopularProducts";
+import Footer from "./Footer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+
+
+export{
+    Hero,
+    PopularProduct,
+    Footer,
+    Services,
+    Subscribe,
+    SuperQuality,
+    SpecialOffer,
+    CustomerReviews,
+}
